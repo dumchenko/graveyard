dd94f1b30d30acbf54e8e24c5f703ca541c4a9a5
Sviatoslav Dumchenko <zcbr95+github@gmail.com>
2022-12-07 17:38:36 +0300

    feat: tune google analytics

---
+++ plugins/google-analytics.js
@@ -0,0 +1,14 @@
+import VueGtag from "vue-gtag";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const router = useRouter();
+  nuxtApp.vueApp.use(
+    VueGtag,
+    {
+      config: {
+        id: "G-GCRK2384VQ",
+      },
+    },
+    router
+  );
+});
