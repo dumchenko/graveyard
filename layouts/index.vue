60787ff06ba6234645a8806a84d50a4aa8154aa3
Sviatoslav Dumchenko <zcbr95+github@gmail.com>
2022-12-08 14:52:15 +0300

    feat: add transitions and eslint

--- layouts/index.vue
+++ layouts/index.vue
@@ -1,44 +1,46 @@
-<script setup>
-const people = await queryContent("people").where({_partial: false}).find()
-const graveyards = await queryContent("graveyards").where({_partial: false}).find()
+<script setup lang="ts">
+const people = await queryContent('people').where({ _partial: false }).find()
+const graveyards = await queryContent('graveyards').where({ _partial: false }).find()
 </script>
 
 <template>
-  <div v-if="people.length">
-    <h1 class="text-center">
-      Люди, которых мы потеряли
-    </h1>
+  <div>
+    <div v-if="people.length">
+      <h1 class="text-center">
+        Люди, которых мы потеряли
+      </h1>
 
-    <CardRows :items="people">
-      <template #image="item">
-        <img :src="`/images/${item.title.toLowerCase()}/${item.photos[0].path}`">
-      </template>
+      <CardRows :items="people">
+        <template #image="item">
+          <img :src="`/images/${item.title.toLowerCase()}/${item.photos[0].path}`">
+        </template>
 
-      <template #title="item">
-        {{ item.name }}
-      </template>
+        <template #title="item">
+          {{ item.name }}
+        </template>
 
-      <template #text="item">
-        {{ item.birthDate }} - {{ item.deathDate }}
+        <template #text="item">
+          {{ item.birthDate }} - {{ item.deathDate }}
 
-        <blockquote v-if="'epitaph' in item">
-          {{ item.epitaph }}
-        </blockquote>
-      </template>
-    </CardRows>
-  </div>
+          <blockquote v-if="'epitaph' in item">
+            {{ item.epitaph }}
+          </blockquote>
+        </template>
+      </CardRows>
+    </div>
 
-  <div v-if="graveyards.length">
-    <hr>
+    <div v-if="graveyards.length">
+      <hr>
 
-    <h1 class="text-center">
-      Кладбища
-    </h1>
+      <h1 class="text-center">
+        Кладбища
+      </h1>
 
-    <CardRows :items="graveyards">
-      <template #title="item">
-        {{ item.name }}
-      </template>
-    </CardRows>
+      <CardRows :items="graveyards">
+        <template #title="item">
+          {{ item.name }}
+        </template>
+      </CardRows>
+    </div>
   </div>
 </template>
